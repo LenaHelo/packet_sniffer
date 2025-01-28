8d19d009687b215112521aeffc3146c2cf1a798d
Lena Helo <lenahelo97@gmail.com>
2025-01-29 00:14:11 +0200

    Added a banner

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
 import signal
 from scapy.all import *
-from functools import partial
+import numpy as np
 
 conf.debug_dissector = True
+sys.path.insert(0, r"./.venv/lib/python3.10/site-packages")
+import tableprint as tp
 
 
-
-
-
+#TODO add 'install tableprint and numpy' to README
 def main():
     flag= True
-
     def CHandler(signal, frame):
         sniffer.stop()
         nonlocal flag
         flag = False
-        print("\n***Sniffing Stopped ***")
+        print("\n***Sniffing Stopped***")
 
     signal.signal(signal.SIGINT, CHandler)
-
     sniffer= AsyncSniffer(prn=lambda x:x.summary(),filter="tcp")
+
+
+
+    tp.banner("Welcome To Packet Sniffer",width=20)
     print("***Sniffing Started*** \nType in ^C when you want to stop")
     sniffer.start()
 
     while flag:
         time.sleep(1)
 
-    print("-FIN-")
 
 if __name__ == "__main__":
 
